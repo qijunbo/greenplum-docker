707390440014fd20a42dcac2b2085d4663990a31
qijunbo <junboqi@foxmail.com>
2018-12-10 18:03:15 +0800

    polish  docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ then
      echo 'Node type='$GP_NODE
     if [ ! -d $MASTER_DATA_DIRECTORY ]; then
         echo 'Master directory does not exist. Initializing master from gpinitsystem_reflect.'
-        yes | cp $HOSTFILE hostlist
+        #yes | cp $HOSTFILE hostlist  # 这一步就不要了, 采用-v参数从外面映射更合适, 这样镜像就不必要重新打包了.
         gpssh-exkeys -f hostlist
         echo "Key exchange complete"
         gpinitsystem -a  -c gpinitsys -s db_standby --su_password=sunway123###
